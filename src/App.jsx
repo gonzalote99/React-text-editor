3813086be5cd25de48df375182776621477d1331
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2024-01-16 19:30:36 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import {useState} from 'react';
+import './App.css';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import 'react-quill/dist/quill.bubble.css';
+import 'react-quill/dist/quill.core.css';
+
+
+const App = () => {
+  const [value, setValue] = useState('');
+
+  const modules = {
+    toolbar: [
+      [{header: [1, 2, 3, 4, 5, 6, false]}],
+      [{font: []}],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        {list: 'ordered'},
+        {list: 'bullet'},
+        {indent: '-1'},
+        {indent: '+1'}
+      ],
+      [{'code-block': {className: 'ql-syntax'}}],
+      ["link", "image", "video"],
+    ]
+  }
+  return(
+    <div className='container-fluid'>
+    <div className='row'>
+    <div className='col-6 editor'>
+    <ReactQuill
+        theme='snow'
+        value={value}
+      onChange={setValue}
+      modules={modules}
+      className='editor-input'
+      />
+    </div>
+      <div className='col-6 preview' dangerouslySetInnerHTML={{__html: value}} />
+      
+     
+    </div>
+    </div>
+  )
+}
+
+export default App;
